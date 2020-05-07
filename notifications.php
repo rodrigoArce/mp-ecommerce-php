1436333ab7da71e90db5354a7a6f5faf6c18bd48
Rodrigo <arcerodrigo3@gmail.com>
2020-05-07 17:57:50 -0300

    updatee

--- notifications.php
+++ notifications.php
@@ -1,3 +1,22 @@
 <?php
-  http_response_code(200);
-?>	
+
+require __DIR__ . '/vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+$merchant_order = null;
+
+switch ($_GET["topic"]) {
+    case "payment":
+        $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+        // Get the payment and the corresponding merchant_order reported by the IPN.
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+        break;
+    case "merchant_order":
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
+        break;
+}
+
+print_r($payment);
+
+?>
