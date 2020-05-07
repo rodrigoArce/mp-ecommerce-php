62aa6dbca8b0c51e4694ae2fd94bf019fbd970f3
Rodrigo <arcerodrigo3@gmail.com>
2020-05-07 17:47:18 -0300

    update

--- notifications.php
+++ notifications.php
@@ -1,17 +1,12 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-http_response_code(200);
+
 switch($_POST["type"]) {
     case "payment":
+        http_response_code(200);
+        
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
         $file = 'logNotification.txt';
     	$current = file_get_contents($file);
