f74af521f73bd08fbd3809d46e2f51f15f40b2c5
Rodrigo <arcerodrigo3@gmail.com>
2020-05-07 17:08:13 -0300

    add token in notification

--- notification.php
+++ notification.php
@@ -1,13 +1,16 @@
 <?php
 
-MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 http_response_code(200);
 
 switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-        var_dump($payment);
+        $file = 'logNotification.txt';
+    	$current = file_get_contents($file);
+    	$current .= $_GET["id"]."\n//////";
+    	file_put_contents($file, $current);
         break;
     case "plan":
         $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
