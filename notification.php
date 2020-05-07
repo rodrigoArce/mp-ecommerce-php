115271954ed8431199dee7da357b70865b6cd648
Rodrigo <arcerodrigo3@gmail.com>
2020-05-07 16:46:14 -0300

    notification created

--- notification.php
+++ notification.php
@@ -1,5 +1,25 @@
 <?php
-	$data = file_get_contents('php://input');
-	$f = fopen('raw.txt', 'a');
-	fwrite($f, $data);
-	fclose($f);
+
+MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
+
+http_response_code(200);
+
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        var_dump($payment);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        break;
+}
+
+?>
+<?php
+	
